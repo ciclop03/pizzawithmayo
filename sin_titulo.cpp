1a24866fe87736881f9c694a221a7410e92ce4ab
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2018-03-20 10:56:55 -0500

    hola1

--- sin_titulo.cpp
+++ sin_titulo.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	count <<"hello, world"<<
+	count <<"hello, world"<<endl;
+	count <<"hola mundo "<<endl;
+	count <<"ya commite"<<endl;
+	count <<"wohhhhhhhhhhh!!!!!!"<<endl;
 	return 0;
 }
 
