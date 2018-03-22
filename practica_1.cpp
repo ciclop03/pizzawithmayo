15ed1d67b7954bad3cca5e9caa9a23421cde7889
ALUMNO-A <ALUMNO-A@UCSP.AULAB>
2018-03-22 09:26:56 -0500

    jhfu

---
+++ practica_1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	float x,y,z;
+	cout<<"introduzca 03 numeros: ";
+	cin>>x>>y>>z;
+	//int	x=5;y=6;
+	int a=x+y;
+	int b=x-y;
+	int c=x*y;
+	float d=x/y;
+	cout<<"el primer numero es: "<<x<<endl;
+	cout<<"el segundo numero es: "<<y<<endl;
+	cout<<"la suma es: "<<a<<endl;
+	cout<<"la resta es: "<<b<<endl;
+	cout<<"el producto es: "<<c<<endl;
+	cout<<"la division es: "<<d<<endl;
+	cout<<"la multiplicación de los 3 es: "<<x*y*z<<endl;
+	
+	return 0;
+}
+// \n \t \r \a \\ \' \"
